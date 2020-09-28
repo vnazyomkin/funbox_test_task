3e7ec737e0c6247d02cd002692275a41b3020a09
Val <val@debian>
2020-09-29 00:13:12 +0300

    Started to add action onClick

--- src/App.js
+++ src/App.js
@@ -4,29 +4,53 @@ import classes from './App.module.css';
 import Card from './Components/Card';
 
 class App extends Component {
+
   state = { 
     food: [
       {
-        taste: 'с фуагра',
+        taste: 'с фуа-гра',
         weight: 0.5,
         action: 'мышь в подарок',
+        count: 3,
+        id:'Food001',
+        selected: false,
       },
       {
         taste: 'с рыбой',
         weight: 2,
         action: '2 мыши в подарок',
+        count: 1,
+        id:'Food002',
+        selected: false,
       },
       {
         taste: 'с курой',
         weight: 5,
         action: '2 мышей в подарок',
         comment: 'заказчик доволен',
+        count: 0,
+        id:'Food003',
+        selected: false,
       }
     ],
   }
+
+  selectCardHandler(id) {
+      const index = this.state.food.findIndex( el => el.id === id );
+      const card = {
+        ...this.state.food[index],
+        selected: true,
+      }; 
+      console.log(card);
+
+  }
+
   render() {
     const Cards = this.state.food.map(card => (
-      <Card {...card}/>
+      <Card 
+        {...card} 
+        key={card.id} 
+        select={(id) => this.selectCardHandler(id)}/>
     ));
     return (
       <main className={classes.App}>
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,17 +2,44 @@ import React from 'react';
 
 import classes from './Card.module.css'
 
-const card = (props) => (
-    <section className={classes.Border}>
-       <div className={classes.Card}>
-            <p className={classes.Additional}>Сказачное заморское яство</p>
-            <h2 className={classes.Name}>Нямушка</h2>
-            <h4 className={classes.Taste}>{props.taste}</h4>
-            <p className={classes.Additional}>{`${props.weight*20} порций`}</p>
-            <p className={classes.Additional}>{props.action}</p>
-            <div className={classes.Cat}></div>
-       </div>
-    </section>
-);
+const card = (props) => {
+    const text = props.count > 0 ? <p className={classes.Text}>Чего сидишь? Порадуй котэ, <a className={classes.link}>купи</a></p> : <p className={classes.Empty}>Печалька, {props.taste} закончился</p>;
+
+    const borderClasses = [classes.Border];
+    const roundClasses = [classes.Round];
+    const cardClasses = [classes.Card];
+
+    if (props.count < 1) {
+        borderClasses.push(classes.Border__empty);
+        roundClasses.push(classes.Round__empty);
+        cardClasses.push(classes.blur);
+    }
+    if (props.selected) {
+        borderClasses.push(classes.Border__selected);
+        roundClasses.push(classes.Round__selected);
+    }
+
+    return (
+        <section>
+            <div className={borderClasses.join(' ')} onClick={(id) => props.select(props.id)}>
+                <div className={cardClasses.join(' ')}>
+                        <p className={classes.Additional}>Сказачное заморское яство</p>
+                        <h2 className={classes.Name}>Нямушка</h2>
+                        <h4 className={classes.Taste}>{props.taste}</h4>
+                        <p className={classes.Additional}>{`${props.weight*20} порций`}</p>
+                        <p className={classes.Additional}>{props.action}</p>
+                        <p className={classes.Additional}>{props.comment}</p>
+                        <div className={classes.Cat}>
+                            <div className={roundClasses.join(' ')}>
+                                <span className={classes.Weight}>{props.weight}</span>
+                                <span className={classes.Kg}>кг</span>
+                            </div>
+                        </div>
+                </div>
+        </div>
+        {text}
+        </section>
+    );
+};
 
 export default card;
