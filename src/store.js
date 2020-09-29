4a693863706311021209655a32f136ce3af9f7a8
Valery Nazemkin <vnazemkin@fil-it.ru>
2020-09-29 17:28:53 +0300

    Finished

---
+++ src/store.js
@@ -0,0 +1,23 @@
+const store = [
+    {
+        taste: 'с фуа-гра',
+        weight: 0.5,
+        action: 'мышь в подарок',
+        selectedText: 'Печень утки разварная с артишоками.'
+    },
+    {
+        taste: 'с рыбой',
+        weight: 2,
+        action: '2 мыши в подарок',
+        selectedText: 'Головы щучьи с чесноком да свежайшая семгушка.'
+    },
+    {
+        taste: 'с курой',
+        weight: 5,
+        action: '2 мышей в подарок',
+        comment: 'заказчик доволен',
+        selectedText: 'Филе из цыплят с трюфельями в бульоне.'
+    }
+];
+
+export default store;
